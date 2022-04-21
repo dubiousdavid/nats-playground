ed3394f1c014b2435e86b135cf65a86ee979227f
David Sargeant <david@dsargeant.com>
2022-04-21 17:03:29 -0400

    Add id to scheduler.

--- src/jobScheduler.js
+++ src/jobScheduler.js
@@ -8,14 +8,15 @@ const debug = _debug('nats')
 
 const connection = await connect()
 const js = connection.jetstream()
+// TODO: Set key prefix per environment
 const redis = new Redis()
 
-const scheduleJob = ({ rule, subject, data }) => {
+const scheduleJob = ({ id, rule, subject, data }) => {
   schedule.scheduleJob(rule, async (date) => {
     debug('SCHEDULING', date)
-    const keyPrefix = 'schedulingLock:'
+    const keyPrefix = 'schedulingLock'
     const scheduledTime = date.getTime().toString()
-    const key = `${keyPrefix}${scheduledTime}`
+    const key = `${keyPrefix}:${id}:${scheduledTime}`
     const lockObtained = await redis.set(key, process.pid, 'PX', ms('1m'), 'NX')
     if (lockObtained) {
       js.publish(subject, data)
--- src/schedulingWithRedis.js
+++ src/schedulingWithRedis.js
@@ -3,6 +3,7 @@ import scheduleJob from './jobScheduler.js'
 const sc = StringCodec()
 
 scheduleJob({
+  id: 'ordersEvery5s',
   rule: '*/5 * * * * *',
   subject: 'ORDERS.job',
   data: sc.encode(process.pid.toString()),
